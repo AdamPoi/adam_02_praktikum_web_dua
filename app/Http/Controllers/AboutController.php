bb77f9216b30da3d3b311f08d8cc0766104deb7e
AdamPoi <isadamrr@gmail.com>
2023-02-24 15:36:18 +0700

    fix(controller): change to singleton

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -4,22 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class PageController extends Controller
+class AboutController extends Controller
 {
   public function index()
-  {
-    return 'Selamat Datang';
-  }
-  public function about()
   {
     return " <h3>NIM  : 2141720185</h3>
                   <h3>Nama  : Adam Rafi Rezandi</h3>
                   <h3>Kelas  : TI 2H </h3>
                 </div>
 ";
-  }
-  public function articles(int $id)
-  {
-    return "Ini merupakan halaman artikel dengan id $id";
   }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
-use App\Http\Controllers\PageController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,6 +21,6 @@ Route::get('/', function () {
   return view('welcome');
 });
 
-Route::get('/', [PageController::class, 'index']);
-Route::get('/about', [PageController::class, 'about']);
-Route::get('/articles/{id}', [PageController::class, 'articles']);
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/about', [AboutController::class, 'index']);
+Route::get('/articles/{id}', [ArticleController::class, 'index']);
